b76fe93d964e454a4603f4e440ac2a4d6ca3e652
Puneet Kaur <kaur.puneet@gmail.com>
2022-09-15 10:54:34 -0600

    Mapping_GeoJSON_Linestrings

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -43,10 +43,21 @@ L.control.layers(baseMaps).addTo(map);
 // Then we add our 'graymap' tile layer to the map.
 //streets.addTo(map); 
 // Accessing the airport GeoJSON URL
-let torontoData = "https://github.com/puneetkaurda/Mapping_Earthquakes./blob/main/torontoRoutes.json";
-// // Grabbing our GeoJSON data.
+let torontoData = "https://raw.githubusercontent.com/puneetkaurda/Mapping_Earthquakes./main/torontoRoutes.json";
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
+
+// Grabbing our GeoJSON data.
 d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+L.geoJSON(data, {
+  style: myStyle,
+  onEachFeature: function(feature, layer) {
+    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "<h3> <hr><h3> Destination: " + feature.properties.dst +"<h3>");
+  }
+}).addTo(map);
 });
